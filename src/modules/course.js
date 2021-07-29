e391dff529f6752c319c37a00de00881da82a1d4
Haneenabonser <aabonser@gmail.com>
2021-07-29 19:06:30 +0300

    socket-caht2 done

--- src/modules/course.js
+++ src/modules/course.js
@@ -1,35 +1,35 @@
-'use strict';
+// 'use strict';
 
-const allCourses = [];
-const Course = require ('../models/student');
+// const allCourses = [];
+// const Course = require ('../models/student');
 
-const createCourse = (id,name ) => {
-  const courseKey = (length = 8) => {
-    return Math.random().toString(10).substr(2, length);
-  };
-  const mycourse = {
-    id = courseKey,
-    name,
-  };
+// const createCourse = (id,name ) => {
+//   const courseKey = (length = 8) => {
+//     return Math.random().toString(10).substr(2, length);
+//   };
+//   const mycourse = {
+//     id = courseKey,
+//     name,
+//   };
 
-  allCourses.push (mycourse);
+//   allCourses.push (mycourse);
 
-  let newCourse= new Course({
-    courseName : name,
-  });
+//   let newCourse= new Course({
+//     courseName : name,
+//   });
 
-  newCourse.save();
-  return mycourse;
-};
+//   newCourse.save();
+//   return mycourse;
+// };
 
-const getCourse = (id) => allCourses.find ((course) => course.id === id );
+// const getCourse = (id) => allCourses.find ((course) => course.id === id );
 
-const updateCourse = (mycourse) => {
-  const index = allCourses.findIndex ((course) => course.id === mycourse.id );
-  if (index !== -1){
-    allCourses[index] = mycourse;
-  }
-};
+// const updateCourse = (mycourse) => {
+//   const index = allCourses.findIndex ((course) => course.id === mycourse.id );
+//   if (index !== -1){
+//     allCourses[index] = mycourse;
+//   }
+// };
 
-module.exports = {allCourses, createCourse, getCourse, updateCourse};
+// module.exports = {allCourses, createCourse, getCourse, updateCourse};
 
