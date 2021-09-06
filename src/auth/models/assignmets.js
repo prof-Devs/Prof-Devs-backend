9294192f8fc274d705a814362c4dcfb16ffb9dfe
DinaSami <dina.faur@yahoo.com>
2021-09-06 20:17:01 +0300

    ok

--- src/auth/models/assignmets.js
+++ src/auth/models/assignmets.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const assignment = new mongoose.Schema({
   title:{type: String, required: true, unique: true},
   text:{type: String, required: true},
-  due_date:{type: Date ,required: true},
+  due_date:{type: String ,required: true},
 //   assignmentFile:{type: Object},
   solution:{type: Array,required:false},// [{email:'email',solution:'solution'}]
   // studentsName: { type: Object,required:false }
